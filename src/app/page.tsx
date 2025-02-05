0c929c3bfefbdd250596f575ff3ce6157c3388c9
AlohaJPM <126013070+alohajpm@users.noreply.github.com>
2025-02-05 05:05:34 -0800

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -106,7 +106,7 @@ const HomePage = () => {
       <div
         className="absolute inset-0 opacity-40 bg-cover bg-center"
         style={{
-          backgroundImage: "url('/nebula.jpg')",
+          backgroundImage: "url('https://onlysnails.com/wp-content/uploads/2025/02/nebula.jpg')",
           backgroundBlendMode: "screen",
         }}
       />
